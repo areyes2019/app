44b4cd6cc8257dd99c6c47c45cdfaf846b8c841d
areyes2019 <reyesabdias@gmail.com>
2022-10-03 09:39:51 -0500

    Se ajusto completamente la tienda

--- resources/views/template/store.blade.php
+++ resources/views/template/store.blade.php
@@ -11,75 +11,315 @@
 <body>
 	<div class="top-header">
 		<div class="container">
-			<p>Bienvenidos a la tienda de sellos más grande de México</p>
-			<p><span class="bi bi-truck"></span>Envíos a todo México</p>
-			<p><span class="bi bi-box-seam"></span>Mas de 300 productos en catálogo</p>
-			<p><span class="bi bi-wallet2"></span>Multiples métodos de pago</p>
+			<div class="row">
+				<div class="col-12 col-md-8 top-links">
+					<div class="header-group">
+						<p><span class="bi bi-send"></span>ventas@sellopronto.com.mx</p>
+	   				</div>
+					<div class="header-group">		
+						<p><span class="bi bi-whatsapp"></span> 461 358 1090</p>
+					</div>	
+				</div>
+				<div class="col-12 col-md-4">
+					<img src="img/bancos.png" class="img-fluid" alt="">
+				</div>
+			</div>
 		</div>
 	</div>
-	<div class="top-header-mobile">
-		<p>Bienvendios a la tinda de sellos mas grande de México</p>
-		<p>vetnas@sellopronto.com.mx</p>
-	</div>
-	<div class="container">
-		<div class="top-menu">
-			<div class="row m-0 p-0">
-				<div class="col-md-5">
-					<div class="store-logo">
-						<a href="{{url('/')}}"><img src="img/logoweb.png" alt=""></a>
+	<div class="main-header">
+		<div class="container">
+			<div class="row p-0 header-container">
+				<div class="col-md-4 col-12 header-logo">
+					<img src="img/logoweb.png" class="img-fluid" alt="">	
+				</div>
+				<div class="col-md-4 offset-4 col-12 header-support">
+					<div class="header-support-group">
+						<div class="support-icon">
+							<span class="bi bi-headset"></span>
+						</div>
+						<div class="support-text">
+							<p>Soporte</p>
+							<p>461 358 1090</p>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-7 side-menu">
-					<div class="menu-link">
-						<a href="">Nosotros</a>
-						<a href="">Contacto</a>
-						<a href="">Políticas</a>
-						<a href="">Envíos</a>
+			</div>
+		</div>
+	</div>											
+	<div class="mobile-menu">
+		<div class="mobile-menu-link">
+			<a href="#mobile-menu" data-bs-toggle="collapse"><span class="bi bi-list"></span></a>
+		</div>
+		<div class="mobile-menu-body collapse" id="mobile-menu">
+			<ul>
+				<li><a href="">Nosotros</a></li>
+				<li><a href="">Contacto</a></li>
+				<li><a href="">Políticas</a></li>
+				<li><a href="">Envíos</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="normal-menu">
+		<div class="container">
+			<div class="normal-menu-list">
+				<ul>
+					<li><a href="">Politicas de envios</a></li>
+					<li><a href="">Nosotros</a></li>
+					<li><a href="">Metodo de compra</a></li>
+					<li><a href="">Contacto</a></li>
+				</ul>
+				<a href=""><span class="bi bi-bag"></span> Cotizador</a>
+			</div>
+		</div>
+	</div>
+	<div class="container categories">
+		<div class="row">
+			<div class="col-12 col-md-3">
+				<div class="categories-menu">
+					<span class="bi bi-list"></span>
+					<p>nuestros sellos</p>
+				</div>
+				<ul class="categories-list">
+					<li><a href="">Sellos con fecha</a></li>
+					<li><a href="">Sellos de madera</a></li>
+					<li><a href="">Sellos de personalizados</a></li>
+					<li><a href="">Sellos de bolsillo</a></li>
+					<li><a href="">Sellos con fecha</a></li>
+					<li><a href="">Sellos de madera</a></li>
+					<li><a href="">Sellos de personalizados</a></li>
+					<li><a href="">Sellos de bolsillo</a></li>
+					
+				</ul>
+			</div>
+			<div class="col-12 col-md-9">
+				<img src="img/banner.png" class="img-fluid" alt="">
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="compromise">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="quality">
+						<p>Cuando hablamos de calidad</p>
+						<p>Hablamos en serio</p>
+						<p>La experiencia de mas de 10 años vendiendo en linea y en el negocio de los sellos de goma, nos repaldan. Nuestro compromiso es que tu producto llegue a tus manos como si lo huberas pedido en persona</p>
+						<button class="btn btn-danger btn-lg">Saber mas</button>
 					</div>
-					<div class="support">
-						<span class="bi bi-headset"></span>
-						<div class="support-text">
-							<p>Atencion personal</p>
-							<p>WhatsApp 461 358 1090</p>
+				</div>
+				<div class="col-md-4">
+					<div class="quality-text">
+						<span class="bi bi-truck"></span>
+						<div class="quality-text-body">
+							<p>Envios Gratis</p>
+							<p>En compras mallores a $1200.00</p>
+						</div>
+					</div>
+					<div class="quality-text">
+						<span class="bi bi-pencil"></span>
+						<div class="quality-text-body">
+							<p>Garantía de diseño</p>
+							<p>Siempre enviamos un diseño previo para tu aprobación</p>
+						</div>
+					</div>
+					<div class="quality-text">
+						<span class="bi bi-box"></span>
+						<div class="quality-text-body">
+							<p>Garantía de calidad</p>
+							<p>Si el producto no cumple con la función, te devolvemos tu dinero</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div id="app">
-		@yield('content')
-	</div>
-	<div class="footer">
-		<div class="row p-0 m-0">
-			<div class="col-md-3 footer-section">
-				<p>Categorias</p>
-				<ul>
-					<li><a href=""><span class="bi bi-arrow-right-circle"></span> Sellos para tela</a></li>
-					<li><a href=""><span class="bi bi-arrow-right-circle"></span> Sellos para doctores</a></li>
-					<li><a href=""><span class="bi bi-arrow-right-circle"></span> Sellos para doctores</a></li>
-					<li><a href=""><span class="bi bi-arrow-right-circle"></span> Sellos para Tela</a></li>
-				</ul>
-			</div>
-			<div class="col-md-3 footer-section">
-				<p>Nosotros</p>
-				<ul>
-					<li><a href="">Contacto</a></li>
-					<li><a href="">Nosotros</a></li>
-					<li><a href="">Politicas</a></li>
-					<li><a href="">Envíos</a></li>
-				</ul>
-			</div>
-			<div class="col-md-3 footer-section">
-				<p class="payment">Metodos de pago</p>
-				<img src="img/oxxopagos.png" class="img-fluid" alt="">
-				<p class="payment">Si eres empresa y requires un estado de cuenta o clave interbancaria, con gusto te la enviamos</p>
-			</div>
-			<div class="col-md-3 footer-section m-0">
-				<img src="img/promesa.png" class="img-fluid" alt="">
+	<div class="container">
+		<div class="block">
+			<div class="row">
+				<div class="col-md-5">
+					<img src="img/grupo.png" class="img-fluid" alt="">
+				</div>
+				<div class="col-md-7 we-are">
+					<p>Nuestros sellos</p>
+					<p>Somo distrubuidores de la linea Colop, con sede en Wels / Austria, y sucursales y participaciones en todo el mundo. Colop es uno de los principales fabricantes internacionales de sellos y modernos dispositivos de marcaje. Todos nuestros productos cuentan con garantía contra defectos de fábrica y los encontrarás a precios competitivos. Contamos tambien con un amplio catálogo de sellos de madera, el tradicional. El personal de Sello Pronto  te ayudará a adquirir los artículos correctos.</p>
+					<p>Nuestros servicio </p>
+					<p>Tanto si eres una empresa, como si eres un particular, nuestro compromiso es brindarte una atención profesional. Nuestro Catalaogo esta diseñado de manera intutiva para que juntos logremos encontrar lo que necesitas</p>
+					<button class="btn btn-danger btn-lg">Quiero cotizar</button>
+				</div>
 			</div>
 		</div>
 	</div>
-	<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+	<div class="container mt-4">
+		<div class="top-ten">
+			<div class="top-ten-title">
+				<p>Te recomendamos</p>
+				<p>nuestro top 10</p>
+			</div>
+			<div class="row">
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C40</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C60</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C35</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C40</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="banner-container">
+			<div class="img-banner">
+				<img src="img/banner-side01.jpg" class="img-fluid" alt="">
+				<button class="btn my-btn-red">Me intresa</button>
+			</div>
+			<div class="img-banner-side">
+				<div class="banner01">
+					<img src="img/banner-side01.jpg" class="img-fluid" alt="">
+					<button class="btn my-btn-red">Me intresa</button>
+				</div>
+				<div class="banner02">
+					<img src="img/banner-side01.jpg" class="img-fluid" alt="">
+					<button class="btn my-btn-red">Me intresa</button>
+				</div>
+			</div>
+		</div>
+	</div>			
+	<!-- categorias destacadas -->
+	<div class="container mt-4">
+		<div class="top-ten">
+			<div class="top-ten-title">
+				<p>Categorías</p>
+				<p>destacadas</p>
+			</div>
+			<div class="row">
+				<div class="col-md-3 p-2 col-12">
+					<div class="card">
+					  <img src="img/banner-side01.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Para Maesros</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn my-btn-red">Me interesa</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3 p-2 col-12">
+					<div class="card">
+					  <img src="img/banner-side01.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Para Doctores</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn my-btn-red">Me interesa</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3 p-2 col-12">
+					<div class="card">
+					  <img src="img/banner-side01.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Para Maesros</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn my-btn-red">Me interesa</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3 p-2 col-12">
+					<div class="card">
+					  <img src="img/banner-side01.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Para Maesros</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn my-btn-red">Me interesa</a>
+					  </div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<!-- prin big-->
+	<div class="container mt-4">
+		<div class="row">
+			<div class="col-md-4 big-img">
+				<img src="" alt="">
+			</div>
+			<div class="col-md-8">
+				<div class="big-title">
+					<p>Queremos que</p>
+					<p>pienses en grande</p>
+					<p>Sus aplicaciones son varias pero las más comunes son para marcar bolsas de tela (aunque también se podrían marcar bolsas de plástico con una tinta especial de secaje rápido) y para marcar cajas de diferentes tipos de negocio.</p>
+					<a href="" class="btn my-btn-red">Me interesa</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- populares-->
+	<div class="container mt-4">
+		<div class="top-ten">
+			<div class="top-ten-title">
+				<p>Nuestros sellos</p>
+				<p>mas populares</p>
+			</div>
+			<div class="row">
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C40</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C60</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C35</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+				<div class="col-md-3 p-2">
+					<div class="top-ten-item">
+						<img src="img/front01.png" class="img-fluid" alt="">
+						<p>Colop C40</p>
+						<p>Sello de 59 x 23 mm</p>
+						<p>$250.00</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
